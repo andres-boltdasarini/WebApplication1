ff1811273a6f775861a09ff473a4f0a7e3534178
Андрей <lau47@mail.ru>
2025-12-25 23:28:11 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace WebApplication1.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Название обязательно")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Название должно быть от 3 до 100 символов")]
+        [Display(Name = "Название")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Цена обязательна")]
+        [Range(1, int.MaxValue, ErrorMessage = "Цена должна быть больше 0")]
+        [Display(Name = "Цена")]
+        public int Price { get; set; }
+
+        [Required(ErrorMessage = "Категория обязательна")]
+        [Display(Name = "Категория")]
+        public string Category { get; set; }
+    }
+}
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Добавляем сервисы в контейнер
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Настраиваем конвейер HTTP-запросов
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
